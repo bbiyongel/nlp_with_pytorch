0efb6934f1b0c82c64fff08b43b3fd9ec53a8a1e
Ki Hyun Kim <khkim@Kiui-MacBook.local>
2019-01-23 07:56:17 +0900

    update code

--- organize.py
+++ organize.py
@@ -49,5 +49,53 @@ def rename():
 
     f.close()
 
+def download_image(url, fn):
+    import urllib.request
+
+    urllib.request.urlretrieve(url, fn)
+
+def rename_images(fn):
+    extract_p = re.compile(r"^.*([0-9]{2})\-[a-z_]+/([0-9]{2})\-.+\.md.*$")
+    chapter_n = extract_p.sub(r'\1', fn)
+    section_n = extract_p.sub(r'\2', fn)
+
+    f = open(fn, 'r')
+
+    cnt = 0
+    for line in f:
+        if line.strip() != '':
+            fn_p = re.compile(r"^.*!\[.+\]\((.+)\).*$")
+
+            if fn_p.search(line):
+                img_fn = fn_p.sub(r'\1', line.strip())
+
+                cnt += 1
+
+                to_fn = '%s-%s-%02d' % (chapter_n, section_n, cnt)
+                print('%s\t%s\t%s' % (fn, img_fn, to_fn))
+
+    f.close()
+
+    return cnt
+
+def rename_images_all():
+    ref_fn = 'SUMMARY.md'
+
+    f = open(ref_fn, 'r')
+
+    cnt = 0
+    for line in f:
+        if line.strip() != '':
+            fn_p = re.compile(r"^.+([0-9]{2}\-[a-z_]+)/([0-9]{2}\-[a-z0-9\-_]+.md).+$")
+
+            if fn_p.search(line):
+                fn = fn_p.sub(r'\1/\2', line.strip())
+
+                cnt += rename_images(fn)
+
+    f.close()
+
+    print(cnt)
+
 if __name__ == '__main__':
-    rename()
+    rename_images_all()
